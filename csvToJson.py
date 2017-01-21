85fc23ec5fb5a5eafa725f8f6ecbfc1ed21937d5
AmitArie <amit.arie@gmail.com>
2017-01-21 20:18:43 +0200

    finished with tagging

--- csvToJson.py
+++ csvToJson.py
@@ -21,7 +21,8 @@ with open("articles_tags_file.json", mode='w', encoding='utf-8') as f:
 
 with open("articles_tags_file.json", mode='w', encoding='utf-8') as feedsjson:
 	articles_list = []
-	for i in range(5):
+	for i in range(len(articles)):
+		print("article number: ", i)
 		article = {}
 		article["name"] = articles[i][ARTICLE_TITLE]
 		article["tag"] = st.tag(articles[i][ARTICLE_BODY].split())
