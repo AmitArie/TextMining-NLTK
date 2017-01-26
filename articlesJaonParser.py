26835e9d6cc7a3c6ad216ec507db110719c92ef7
AmitArie <amit.arie@gmail.com>
2017-01-26 17:59:13 +0200

    moving to mac

--- articlesJaonParser.py
+++ articlesJaonParser.py
@@ -1,6 +1,40 @@
+#!/usr/bin/python3
+
 import json
+from nltk.tag import StanfordNERTagger
+st = StanfordNERTagger('english.all.3class.distsim.crf.ser.gz')
+
+
+TAG_TYPE = 1
+
+def get_companies_from_tags(tags):
+	companies = []
+	for tag in tags:
+		if tag[TAG_TYPE] == "ORGANIZATION":
+			companies.append(tag)
+	return companies
+
+
+
+def get_companies_from_article (article):
+	companies = {}
+	companies["title_companies"] = get_companies_from_tags(st.tag(article["name"].split()))
+	companies["body_companies"] = get_companies_from_tags(article["tag"])
+
+	return companies
+
 
 with open('articles_tags_file.json') as data_file:    
     data = json.load(data_file)
-    for entry in data:
-    	print(json.loads(entry)["tag"])
+    # for article_number in range(len(data)):
+    for article_number in range(1):
+    	article_json = json.loads(data[article_number])
+    	print(get_companies_from_article(article_json))
+
+
+
+
+
+
+
+
--- csvToJson.py
+++ csvToJson.py
@@ -3,6 +3,8 @@
 import json
 import pandas as pd
 from nltk.tag import StanfordNERTagger
+import nltk as nltk
+
 st = StanfordNERTagger('english.all.3class.distsim.crf.ser.gz')
 
 df = pd.read_csv('Full.csv', delimiter=',')
@@ -15,20 +17,33 @@ ARTICLE_DATE = 4
 ARTICLE_TITLE = 5
 ARTICLE_BODY = 6
 
+def tag_atricle_sentence(article):
+	sent_tags = {}
+	sent  = nltk.sent_tokenize(articles[0][ARTICLE_BODY])
+
+	for i in range(len(sent)):
+		sent_tags[str(i)] = []
+		sent_tags[str(i)] = st.tag(sent[i].split())
+
+	return sent_tags
+
+
 with open("articles_tags_file.json", mode='w', encoding='utf-8') as f:
     json.dump([], f)
 
 
+
 with open("articles_tags_file.json", mode='w', encoding='utf-8') as feedsjson:
 	articles_list = []
+	
+
 	for i in range(len(articles)):
 		print("article number: ", i)
 		article = {}
 		article["name"] = articles[i][ARTICLE_TITLE]
-		article["tag"] = st.tag(articles[i][ARTICLE_BODY].split())
+		article["sent_tag"] = tag_atricle_sentence(articles[i][ARTICLE_BODY])
 		json_data = json.dumps(article)
 		articles_list.append(json_data)
-		
 	json.dump(articles_list, feedsjson)
 
 
