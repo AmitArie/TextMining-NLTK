ab2908a0a0c61d663a6f297fa391ad6e02a4588c
Amit Arie <amit.arie@gmail.com>
2017-01-28 12:04:58 +0200

    starting new

--- articlesJaonParser.py
+++ articlesJaonParser.py
@@ -5,23 +5,6 @@ from nltk.tag import StanfordNERTagger
 st = StanfordNERTagger('english.all.3class.distsim.crf.ser.gz')
 
 
-TAG_TYPE = 1
-
-def get_companies_from_tags(tags):
-	companies = []
-	for tag in tags:
-		if tag[TAG_TYPE] == "ORGANIZATION":
-			companies.append(tag)
-	return companies
-
-
-
-def get_companies_from_article (article):
-	companies = {}
-	companies["title_companies"] = get_companies_from_tags(st.tag(article["name"].split()))
-	companies["body_companies"] = get_companies_from_tags(article["tag"])
-	return companies
-
 
 def get_main_company_from_article(article):
 	article_companies = get_companies_from_article(article)
@@ -30,15 +13,20 @@ def get_main_company_from_article(article):
 
 with open('articles_tags_file.json') as data_file:    
     data = json.load(data_file)
-    print("Length: ", len(data))
+
     # for article_number in range(len(data)):
-    for article_number in range(1):
+    for article_number in range(10):
     	article_json = json.loads(data[article_number])
     	print("article name: ", article_json["name"])
 
     	for key  in article_json["sent_tag"]:
     		print("sent number: ", key , "  " , article_json["sent_tag"][key])
     		print("##################################################")
+    	print("  ")
+    	print("  ")
+    	print("  ")
+    	print("  ")
+    	print("  ")
 
 
 
--- csvToJson.py
+++ csvToJson.py
@@ -5,6 +5,8 @@ import pandas as pd
 from nltk.tag import StanfordNERTagger
 import nltk as nltk
 
+
+
 st = StanfordNERTagger('english.all.3class.distsim.crf.ser.gz')
 
 df = pd.read_csv('Full.csv', delimiter=',')
@@ -19,7 +21,7 @@ ARTICLE_BODY = 6
 
 def tag_atricle_sentence(article):
 	sent_tags = {}
-	sent  = nltk.sent_tokenize(articles[0][ARTICLE_BODY])
+	sent  = nltk.sent_tokenize(article)
 
 	for i in range(len(sent)):
 		sent_tags[str(i)] = []
@@ -37,13 +39,15 @@ with open("articles_tags_file.json", mode='w', encoding='utf-8') as feedsjson:
 	articles_list = []
 	
 
-	for i in range(len(articles)):
+	for i in range(0, 10):
 		print("article number: ", i)
 		article = {}
 		article["name"] = articles[i][ARTICLE_TITLE]
 		article["sent_tag"] = tag_atricle_sentence(articles[i][ARTICLE_BODY])
 		json_data = json.dumps(article)
 		articles_list.append(json_data)
+
+		print(json_data)
 	json.dump(articles_list, feedsjson)
 
 
